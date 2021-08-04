6be83d21f2e4444098a986e9a09f5790f0587f57
unknown <gersonforester89@gmail.con>
2021-08-04 21:14:31 +0700

    popup

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("HALO..YANA....");  
+alert("Lagi Ngapain...");
+alert("udah ngopi belum..wwkwkw");
+alert("YANA....!!!");
+alert("Aku cuman mau bilang");
+alert("klo aku mulai");
+alert("kagum sama Kamu");
+alert("wkwkwkw bercandah")
+alert("tapi serius..");
+alert("tolong dijawab ya");
+alert("klo suka tulis kata SUKA yach ");
+var nama = prompt("TULIS DISINI");
+alert("tulis SUKA dong...");
+var nama = prompt ("yuk seriusin dong...");
+alert("ya udah makasi, YANA.....wkwkwk");
+
+
